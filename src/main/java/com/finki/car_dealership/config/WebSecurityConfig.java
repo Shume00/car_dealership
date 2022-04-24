0066e6e54cac8c3465ab8f3dc950288916f9a2ac
Andrej Shumkovski <85301264+Shume00@users.noreply.github.com>
2022-04-24 22:21:57 +0200

    images work
    images pass through security
    fixed navbar on other pages

--- src/main/java/com/finki/car_dealership/config/WebSecurityConfig.java
+++ src/main/java/com/finki/car_dealership/config/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
                     http.csrf().disable()
                             .authorizeRequests()
-                            .antMatchers("/","/home","/resources/**","/api/**","/register").permitAll()
+                            .antMatchers("/","/home","/resources/**","/images/**","/api/**","/register").permitAll()
                             .antMatchers("/admin/**").hasRole("ADMIN")
                             .anyRequest()
                             .authenticated()
